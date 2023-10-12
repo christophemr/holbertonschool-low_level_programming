adaf31e07a514f37bd197b6134cbfd9dcede4e3b
christophemr <christophemr59@gmail.com>
2023-10-12 05:56:10 -0700

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-/**	
+/**
  * main - entry point function
  *
- * Return 0 on succes
+ * Return: 0 on succes
  *
- * */
+ */
 int main(void)
-{	
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
