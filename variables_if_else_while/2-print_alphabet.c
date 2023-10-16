22abcfcc3a4cb6d589b1d2c16f7641a7a74d200f
christophemr <christophemr59@gmail.com>
2023-10-16 04:37:29 -0700

    2-print_alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+        char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	return (0);
+}
