0850d3c763ab4cc1385d6c9843e6b182cbe1b8ce
christophemr <christophemr59@gmail.com>
2023-10-16 07:33:08 -0700

    6-print_numberz

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -9,8 +9,8 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num <= 10; num++)
-		putchar(num % 10);
+	for (num = 0; num < 10; num++)
+		putchar((num % 10 + '10');
 	putchar('\n');
 	return (0);
 }
