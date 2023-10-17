d133dba1a21c35adeacdac4999236a9a4e89c263
christophemr <christophemr59@gmail.com>
2023-10-17 07:40:07 -0700

    3-islower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _islower - test for lowercase
+ *
+ * @c: character to verify
+ * Return: 1 on success
+ */
+
+int _islower(int c)
+{
+	int letter;
+	letter = 'c';
+
+	if (_islower(letter))
+
+	if (c >= 'a' && c <= 'z')
+		return (1);
+
+	return (0);
+
+}
