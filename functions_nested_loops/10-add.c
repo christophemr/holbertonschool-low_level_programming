cf4c5ae20bdbd07418eca2271d8b680ae839079c
christophemr <christophemr59@gmail.com>
2023-10-18 06:24:45 -0700

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add- function that add the integers
+ * Return: return the result of the function
+ *@a: number to add
+ *@b: number to add
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
