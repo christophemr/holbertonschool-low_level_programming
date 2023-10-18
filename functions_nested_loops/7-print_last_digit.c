378051eab24418a36bb0dafc06e18e569433e0c7
christophemr <christophemr59@gmail.com>
2023-10-18 03:09:17 -0700

    7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -10,10 +10,11 @@ int print_last_digit(int i)
 {
 
 	if (i < 0)
-		i = -i;
+		i = (-i % 10);
 
-	i = i % 10;
-		_putchar(i + '0');
-		return (i);
+	else
+		i = i % 10;
+	_putchar(i + '0');
+	return (i);
 
 }
