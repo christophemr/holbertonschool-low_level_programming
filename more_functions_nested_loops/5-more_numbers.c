e4a4c58a2341486480642de49695c1f68bd67fd6
christophemr <christophemr59@gmail.com>
2023-10-20 05:51:03 -0700

    5-more_numbers

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; 1 < 9 ; i++)
+	{
+		for (j = 0 ; j <= 14 ; j++)
+		{
+			_putchar(j + '0');
+		}
+	}
+	_putchar('\n');
+}
