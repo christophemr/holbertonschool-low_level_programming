fbb278a97cca582eec681f0fd5999840a348b7c7
christophemr <christophemr59@gmail.com>
2023-10-23 00:36:41 -0700

    0-98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes pointer to an int and updates its value to 98
+ * @n: pointer
+ * Return: 0 on success
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
