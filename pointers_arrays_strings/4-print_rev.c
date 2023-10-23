f2d87340e9b9375bf57296154f4df1071f6a5adb
christophemr <christophemr59@gmail.com>
2023-10-23 02:58:28 -0700

    4-print_rev

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - print the string in reverse
+ * @s: string to print
+ */
+
+void print_rev(char *s)
+{
+
+	int i, j, k;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	k = i;
+
+	for (j = k - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+}
