998fb8ea77a9d5efd2fd45dd7141aaebf7f1d294
christophemr <christophemr59@gmail.com>
2023-10-25 03:17:23 -0700

    1-strncat

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *_strncat - concatenate 2 strings
+ *@dest: string to concatenate
+ *@src: string to append
+ *Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0' && n > 0)
+	{
+
+	dest[i] = src[j];
+	i++;
+	j++;
+	n--;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
