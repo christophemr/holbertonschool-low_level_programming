577a488c178de218ad9886bef9e1dd5c116771a5
christophemr <christophemr59@gmail.com>
2023-11-15 02:43:17 -0800

    2-print_dog fix

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -14,6 +14,6 @@ void print_dog(struct dog *d)
 	printf("name: %s\n", (d->name) ? d->name : "(nil)");
 	printf("age: %f\n", (d->age) ? d->age : 0);
 	printf("owner: %s\n", (d->owner) ? d->owner : "(nil)");
-	}	
-	return;
+	}
+
 }
