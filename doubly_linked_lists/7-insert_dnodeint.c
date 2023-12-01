bc45a33e7ba90857e39d276b15b2ebcd38971eb6
christophemr <christophemr59@gmail.com>
2023-12-01 02:44:59 -0800

    fix

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -47,7 +47,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (current->next != NULL)
 	{
 		current->prev = new_node;
-	}	
+	}
 	current->next = new_node;
 	return (new_node);
 }
