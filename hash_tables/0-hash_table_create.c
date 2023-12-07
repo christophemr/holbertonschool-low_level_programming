c683458e33f886f6480d5a227aed9a1e71608494
christophemr <christophemr59@gmail.com>
2023-12-07 05:16:46 -0800

    fix size of array

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -13,7 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	hashTable->size = size;
-	hashTable->array = malloc(sizeof(size));
+	hashTable->array = malloc(sizeof(hash_node_t) * size);
 	if (hashTable->array == NULL)
 		return (NULL);
 
